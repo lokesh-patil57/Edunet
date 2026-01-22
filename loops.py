a70869a41ead5178171b7d89f3ea8d021df38900
Lucky Patil <luckypatil577R@gmail.com>
2026-01-22 12:21:32 +0530

    Loops in Python

---
+++ loops.py
@@ -0,0 +1,14 @@
+#tables code
+
+print("for Loop Example:")
+print("Multiplication Table of 2:")
+
+for i in range(2, 11):
+    print(f"2 x {i} = {2 * i}") 
+
+print("while Loop Example:")
+
+i = 0
+while i<5:
+    print("Lucky")
+    i=i+1
