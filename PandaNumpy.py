363ba35450b3510a5e921bfb29b183a2fbf2c864
Lucky Patil <luckypatil577R@gmail.com>
2026-01-28 23:10:03 +0530

    graphs and charts

--- PandaNumpy.py
+++ PandaNumpy.py
@@ -31,11 +31,45 @@ print(f"Mean Cost (Million $): {cost_mean}")
 
 #Impute missing values in energy Consumption(MWh) with mean
 
-energy_df["Energy comsumption (MWh)"].fillna(ec_mean, inplace=True)
+energy_df["Energy comsumption (MWh)"] = energy_df["Energy comsumption (MWh)"].fillna(ec_mean)
 
 #Impute missing values in cost(Million $) with mean
-energy_df["cost(Million $)"].fillna(cost_mean, inplace=True)
+energy_df["cost(Million $)"] = energy_df["cost(Million $)"].fillna(cost_mean)
 
 print("\nEnergy DataFrame after Imputing Missing Values with Mean:")
 energy_df.head()
 print(energy_df)
+
+#Forward Filling
+forward_filled_df = energy_df.ffill()
+print("Data after Forward Filling")
+print(forward_filled_df)
+
+#Backward Filling
+backward_filled_df = energy_df.bfill()
+print("Data after Backward Filling")
+print(backward_filled_df)
+
+#Data Normalization (Min-Max Scaling)
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+energy_df[["Energy comsumption (MWh)", "cost(Million $)"]] = scaler.fit_transform(energy_df[["Energy comsumption (MWh)", "cost(Million $)"]])
+
+print("\n Data after Normalization (Min-Max Scaling):")
+print(energy_df)
+
+
+#standardization (Z-Score Scaling)
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+energy_df[["Energy comsumption (MWh)", "cost(Million $)"]] = scaler.fit_transform(energy_df[["Energy comsumption (MWh)", "cost(Million $)"]])
+
+print("\n Data after Standardization (Z - Score Scaling):")
+print(energy_df)
+
+#One-Hot Encode the 'energy source' column
+one_hot_encoded_df = pd.get_dummies(energy_df, columns=["Energy source"])
+print("\nData after One-Hot Encoding Categorial Variable:")
+print(one_hot_encoded_df)
